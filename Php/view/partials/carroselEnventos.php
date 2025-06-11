b9f2dec208f6f2bca721ca70f4105d8ad846b04d
Ville Nunes Figueiredo da Silva <104937599+VilleNunes@users.noreply.github.com>
2025-06-11 18:37:56 -0300

    Iniciando paginas de home, eventos e login

---
+++ Php/view/partials/carroselEnventos.php
@@ -0,0 +1,122 @@
+
+<div class=" bg-gray-300/80 py-">
+    <div class="w-full max-w-6xl mx-auto px-4 py-10 relative">
+        <h2 class="text-2xl font-bold mb-6">Eventos</h2>
+
+        <!-- Botões -->
+        <button id="scroll-left"
+            class="absolute left-0 top-1/2 -translate-y-1/2 z-[0] bg-white shadow-md p-2 rounded-full hover:bg-gray-100">
+            ❮
+        </button>
+        <button id="scroll-right"
+            class="absolute right-0 top-1/2 -translate-y-1/2 z-[0] bg-white shadow-md p-2 rounded-full hover:bg-gray-100">
+            ❯
+        </button>
+
+        <!-- Container do carrossel -->
+        <div id="carousel" class="flex gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory scrollbar-hide px-6">
+            <!-- Card 1 -->
+            <div class="snap-start min-w-[350px] bg-white shadow-md rounded-lg p-4">
+                <img src="logoAlfa.png" class="rounded-md mb-4 h-[200px] object-contain" alt="Card 1">
+                <h3 class="font-semibold text-lg">Titulo</h3>
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                    veritatis fugit architecto sint pariatur quas nostrum delectus sit iste quis. Fuga quibusdam
+                    rerum
+                    nam delectus repudiandae provident magnam necessitatibus. Molestiae.</p>
+                    <p class="mt-3">Data: 20/30/2024</p>
+                    
+                <button class="btn btn-neutral mt-5">Ver Evento</button>
+            </div>
+
+            <!-- Card 2 -->
+            <div class="snap-start min-w-[350px] bg-white shadow-md rounded-lg p-4">
+                <img src="logoAlfa.png" class="rounded-md mb-4 h-[200px] object-contain" alt="Card 1">
+                <h3 class="font-semibold text-lg">Titulo</h3>
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                    veritatis fugit architecto sint pariatur quas nostrum delectus sit iste quis. Fuga quibusdam
+                    rerum
+                    nam delectus repudiandae provident magnam necessitatibus. Molestiae.</p>
+                    <p class="mt-3">Data: 20/30/2024</p>
+                    <p>Localização: Rua 3</p>
+                    
+                <button class="btn btn-neutral mt-5">Ver Evento</button>
+            </div>
+            <div class="snap-start min-w-[350px] bg-white shadow-md rounded-lg p-4">
+                <img src="logoAlfa.png" class="rounded-md mb-4 h-[200px] object-contain" alt="Card 1">
+                <h3 class="font-semibold text-lg">Titulo</h3>
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                    veritatis fugit architecto sint pariatur quas nostrum delectus sit iste quis. Fuga quibusdam
+                    rerum
+                    nam delectus repudiandae provident magnam necessitatibus. Molestiae.</p>
+                    <p class="mt-3">Data: 20/30/2024</p>
+                    <p>Localização: Rua 3</p>
+                    
+                <button class="btn btn-neutral mt-5">Ver Evento</button>
+            </div>
+            <div class="snap-start min-w-[350px] bg-white shadow-md rounded-lg p-4">
+                <img src="logoAlfa.png" class="rounded-md mb-4 h-[200px] object-contain" alt="Card 1">
+                <h3 class="font-semibold text-lg">Titulo</h3>
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                    veritatis fugit architecto sint pariatur quas nostrum delectus sit iste quis. Fuga quibusdam
+                    rerum
+                    nam delectus repudiandae provident magnam necessitatibus. Molestiae.</p>
+                    <p class="mt-3">Data: 20/30/2024</p>
+                    <p>Localização: Rua 3</p>
+                    
+                <button class="btn btn-neutral mt-5">Ver Evento</button>
+            </div>
+            <div class="snap-start min-w-[350px] bg-white shadow-md rounded-lg p-4">
+                <img src="logoAlfa.png" class="rounded-md mb-4 h-[200px] object-contain" alt="Card 1">
+                <h3 class="font-semibold text-lg">Titulo</h3>
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                    veritatis fugit architecto sint pariatur quas nostrum delectus sit iste quis. Fuga quibusdam
+                    rerum
+                    nam delectus repudiandae provident magnam necessitatibus. Molestiae.</p>
+                    <p class="mt-3">Data: 20/30/2024</p>
+                    <p>Localização: Rua 3</p>
+                    
+                <button class="btn btn-neutral mt-5">Ver Evento</button>
+            </div>
+            <div class="snap-start min-w-[350px] bg-white shadow-md rounded-lg p-4">
+                <img src="logoAlfa.png" class="rounded-md mb-4 h-[200px] object-contain" alt="Card 1">
+                <h3 class="font-semibold text-lg">Titulo</h3>
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                    veritatis fugit architecto sint pariatur quas nostrum delectus sit iste quis. Fuga quibusdam
+                    rerum
+                    nam delectus repudiandae provident magnam necessitatibus. Molestiae.</p>
+                    <p class="mt-3">Data: 20/30/2024</p>
+                    <p>Localização: Rua 3</p>
+                    
+                <button class="btn btn-neutral mt-5">Ver Evento</button>
+            </div>
+
+            <div class="snap-start min-w-[350px] bg-white shadow-md rounded-lg p-4">
+                <img src="logoAlfa.png" class="rounded-md mb-4 h-[200px] object-contain" alt="Card 1">
+                <h3 class="font-semibold text-lg">Titulo</h3>
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                    veritatis fugit architecto sint pariatur quas nostrum delectus sit iste quis. Fuga quibusdam
+                    rerum
+                    nam delectus repudiandae provident magnam necessitatibus. Molestiae.</p>
+                    <p class="mt-3">Data: 20/30/2024</p>
+                    <p>Localização: Rua 3</p>
+                    
+                <button class="btn btn-neutral mt-5">Ver Evento</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    const carousel = document.getElementById("carousel");
+    const leftBtn = document.getElementById("scroll-left");
+    const rightBtn = document.getElementById("scroll-right");
+
+    const scrollAmount = 300;
+
+    leftBtn.addEventListener("click", () => {
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+
+    rightBtn.addEventListener("click", () => {
+        carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+</script>
---
+++ Php/view/partials/faixa.php
@@ -0,0 +1,3 @@
+<div class="bg-black font-bold text-white text-center p-1">
+    @unialfa
+</div>
